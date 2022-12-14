c94bdcc68add560e4372f2e0c00a11b19db05ad3
minhaj539 <mdminhaj8383@gmail.com>
2022-12-14 11:10:19 +0530

    updated

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,10 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
+  int price;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-
+       addExtraCheese();
+       addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int price,chPrice,tpPrice,bagPrice;
     private Boolean isVeg;
 
     private boolean cheese,topping,bag;
@@ -13,12 +13,17 @@ public class Pizza {
         this.topping=false;
         this.bag=false;
         this.isVeg = isVeg;
+        this.chPrice=80;
+        this.bagPrice=20;
         if(isVeg){
             this.price=300;
+            this.tpPrice=70;
         }
         else{
             this.price=400;
+            this.tpPrice=120;
         }
+        this.bill="Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -27,28 +32,37 @@ public class Pizza {
 
     public void addExtraCheese(){
         if(cheese==false) {
-            price += 80;
+            price += chPrice;
             cheese=true;
         }
     }
 
     public void addExtraToppings(){
         if(topping==false) {
-            if (isVeg) price += 70;
-            else price += 120;
+            if (isVeg) price += tpPrice;
+            else price += tpPrice;
             topping=true;
         }
     }
 
     public void addTakeaway(){
         if(bag==false) {
-            price += 20;
+            price += bagPrice;
             bag=true;
         }
     }
 
     public String getBill(){
-        bill=String.valueOf(price);
+      if(cheese==true){
+          this.bill=this.bill+"Extra Cheese Added: "+this.chPrice+"\n";
+      }
+      if(topping==true){
+          this.bill=this.bill+"Extra Toppings Added: "+this.tpPrice+"\n";
+      }
+      if(bag==true){
+          this.bill=this.bill+"Paperbag Added: "+this.bagPrice+"\n";
+      }
+          this.bill=this.bill+ "Total Price: "+this.price+"\n";
         return this.bill;
     }
 }
