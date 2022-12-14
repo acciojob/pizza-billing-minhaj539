50f19afa1c11d1dc404836caba689fd814e5d62d
minhaj539 <mdminhaj8383@gmail.com>
2022-12-14 11:21:12 +0530

    updated

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -15,7 +15,7 @@ public class Pizza {
         this.isVeg = isVeg;
         this.chPrice=80;
         this.bagPrice=20;
-        if(isVeg){
+        if(this.isVeg){
             this.price=300;
             this.tpPrice=70;
         }
@@ -31,35 +31,35 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        if(cheese==false) {
-            price += chPrice;
-            cheese=true;
+        if(this.cheese==false) {
+            this.price += this.chPrice;
+            this.cheese=true;
         }
     }
 
     public void addExtraToppings(){
-        if(topping==false) {
-            if (isVeg) price += tpPrice;
-            else price += tpPrice;
-            topping=true;
+        if(this.topping==false) {
+            if (isVeg) this.price += tpPrice;
+            else this.price += tpPrice;
+            this.topping=true;
         }
     }
 
     public void addTakeaway(){
-        if(bag==false) {
-            price += bagPrice;
-            bag=true;
+        if(this.bag==false) {
+            this.price += bagPrice;
+            this.bag=true;
         }
     }
 
     public String getBill(){
-      if(cheese==true){
+      if(this.cheese==true){
           this.bill=this.bill+"Extra Cheese Added: "+this.chPrice+"\n";
       }
-      if(topping==true){
+      if(this.topping==true){
           this.bill=this.bill+"Extra Toppings Added: "+this.tpPrice+"\n";
       }
-      if(bag==true){
+      if(this.bag==true){
           this.bill=this.bill+"Paperbag Added: "+this.bagPrice+"\n";
       }
           this.bill=this.bill+ "Total Price: "+this.price+"\n";
